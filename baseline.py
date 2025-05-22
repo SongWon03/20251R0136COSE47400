b6809d4bdd4c966c6d953a7448ad9680e458c251
SongWon03 <146622123+SongWon03@users.noreply.github.com>
2025-05-22 16:48:55 +0900

    Update baseline.py
    Add FC layer, change cosine similarity function

--- baseline.py
+++ baseline.py
@@ -1,33 +1,36 @@
 from pycocotools.coco import COCO
 from sklearn.feature_extraction.text import TfidfVectorizer
 import torch
+import torch.nn as nn
 import torchvision.models as models
 import torchvision.transforms as transforms
 import os
 from PIL import Image
-from sklearn.decomposition import TruncatedSVD
 from sklearn.metrics.pairwise import cosine_similarity
-import pandas as pd
 import numpy as np
 from IPython.display import display
 
 
-class TF_IDF(object):
-    def __init__(self, data_type, max_features, use_embedding=False):
+class TF_IDF(nn.Module):
+    def __init__(self, data_type, max_features, embed_dim=512, use_fc=False, device=None):
+        super().__init__()
         self.data_type = data_type
         self.data_dir = '/content/coco/'
         self.ann_file = '{}/annotations/annotations/captions_{}.json'.format(self.data_dir, data_type)
         self.coco_caps = COCO(self.ann_file)
         self.img_ids = self.coco_caps.getImgIds()
         self.max_features = max_features
-        self.use_embedding = use_embedding
+        self.embed_dim = embed_dim
+        self.use_fc = use_fc
+        self.device = device or ('cuda' if torch.cuda.is_available() else 'cpu')
         self.vectorizer = TfidfVectorizer(
             max_features= self.max_features,
             ngram_range=(1, 1),   # use uni-gram
             lowercase=True,
             stop_words='english')
-        self.svd = None
-      
+        if self.use_fc:
+            self.fc = nn.Linear(self.max_features, self.embed_dim, bias=True).to(self.device)
+
     def fit_tfidf(self):
         captions = []
         ids_for_matrix = []
@@ -37,22 +40,21 @@ class TF_IDF(object):
             text = ' '.join([ann['caption'] for ann in anns])
             captions.append(text)
             ids_for_matrix.append(img_id)
-        # apply TF-IDF encoding
+        # apply TF-IDF encoding & FC layer
         tfidf_matrix = self.vectorizer.fit_transform(captions)
-        if self.use_embedding:
-            self.svd = TruncatedSVD(n_components=512, random_state=42)
-            tfidf_matrix = self.svd.fit_transform(tfidf_matrix)
-        return tfidf_matrix, ids_for_matrix
+        if self.use_fc:
+            tfidf_dense = torch.from_numpy(tfidf_matrix.toarray()).float().to(self.device)
+            emb = self.fc(tfidf_dense)
+        return emb.detach().cpu().numpy(), ids_for_matrix
 
     def encode_query(self, query_text):
         # apply TF-IDF encoding to query
         q_tfidf = self.vectorizer.transform([query_text])
-        if self.use_embedding:
-            if self.svd is None:
-                raise RuntimeError('Call fit_tfidf(use_embedding=True) before encoding query.')
-            return self.svd.transform(q_tfidf)
-        else:
-            return q_tfidf
+        if self.use_fc:
+            q_dense = torch.from_numpy(q_tfidf.toarray()).float().to(self.device)
+            q_emb = self.fc(q_dense)
+            return q_emb.detach().cpu().numpy()
+        return q_tfidf
 
 
 class ImageEncoder(object):
@@ -88,19 +90,24 @@ class ImageEncoder(object):
                 feature = self.model(batch).squeeze()  # (512,)
             img_features.append(feature.cpu())
         return img_features
-    
-    
-def cos_similarity(data_type, img_features, ids_for_matrix, query_vector):
-    img_feats_np = np.stack([f.numpy() for f in img_features])
-    similarities = cosine_similarity(query_vector, img_feats_np)[0]
+
+
+def q_cos_similarity(data_type, text_encoder, feature_source, ids_for_matrix, query_text, top_k=5):
+    q_vec = text_encoder.encode_query(query_text)
+    # feature source to numpy array
+    if isinstance(feature_source, np.ndarray):
+        features = feature_source
+    else:
+        features = np.stack([f.cpu().numpy() for f in feature_source])
+    similarities = cosine_similarity(q_vec, features)[0]
     # select top 5 results
-    top5_idx = similarities.argsort()[-5:][::-1]
-    top5_img_ids = [ids_for_matrix[i] for i in top5_idx]
-    top5_scores = similarities[top5_idx]
+    top_idx = similarities.argsort()[-top_k:][::-1]
+    top_img_ids = [ids_for_matrix[i] for i in top_idx]
+    top_scores = similarities[top_idx]
     # visualize
     data_dir = '/content/coco'
     img_dir = '{}/{}/{}'.format(data_dir, data_type, data_type)
-    for img_id, score in zip(top5_img_ids, top5_scores):
+    for img_id, score in zip(top_img_ids, top_scores):
         filename = f"{img_id:012d}.jpg"
         path = os.path.join(img_dir, filename)
         img = Image.open(path).convert('RGB')
